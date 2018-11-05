26224f309a7646f243d01669e02c01e5769a5a97
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-11-05 06:06:40 +0100

    html previewer 👀

--- public/data.js
+++ public/data.js
@@ -1,5 +1,7 @@
 const api = document.location.origin + '/';
 const dataForm = document.getElementById('dataForm');
+var code = document.getElementById('code').contentWindow.document;
+var html = document.getElementById('omschrijving');
 
 const sendData = e => {
   e.preventDefault();
@@ -22,6 +24,16 @@ const sendData = e => {
       //  location.reload();
     }
   });
+};
+
+code.open();
+  code.writeln(html.value);
+  code.close();
+
+document.body.onkeyup = function () {
+  code.open();
+  code.writeln(html.value);
+  code.close();
 };
 
 dataForm.addEventListener('submit', sendData);
